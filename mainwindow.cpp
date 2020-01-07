cde1b4ac1dbb71423e3552e0437dd00ae0e2b473
bouhssini <bouhssini@gmail.com>
2020-01-07 17:30:18 +0100

    edit 2020-01-07 17:30

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,9 +13,52 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::Senthash()
+{
+   // ui->progressBar->setValue(0);
+
+
+}
+
+void MainWindow::AddPart(QHttpMultiPart *multiPart, QString name, QByteArray by)
+{
+    QHttpPart textPart;
+    textPart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                       QVariant("form-data; name=\""+name+"\""));
+    textPart.setBody(by);
+    multiPart->append(textPart);
+}
+
 void MainWindow::chimage(QByteArray bzip, QString hMD5)
 {
     ui->wSa->Imageunzip(bzip);
+
+    /*-------------------------------------------------------------------------*/
+    QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+
+    AddPart(multiPart,"id_usr","1");
+    AddPart(multiPart,"id_room","20");
+
+    QHttpPart imagePart;
+    imagePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/*"));
+    imagePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                        QVariant("form-data; name=\"userImage\";  filename=\""+hMD5+"\""));
+
+    imagePart.setBody(bzip);
+
+    multiPart->append(imagePart);
+
+    QUrl url("http://66.70.190.62/web/upload/upload.php");
+    QNetworkRequest request(url);
+
+    QNetworkAccessManager * manager = new QNetworkAccessManager();
+    reply = manager->post(request, multiPart);
+    multiPart->setParent(reply); // delete the multiPart with the reply
+    connect(reply, SIGNAL(uploadProgress(qint64,qint64)), this, SLOT(Progress(qint64,qint64)));
+    connect(reply, SIGNAL(finished()), this, SLOT(uploadDone()));
+    /*-------------------------------------------------------------------------*/
+
+
 }
 
 void MainWindow::on_checkBox_clicked(bool checked)
@@ -23,3 +66,8 @@ void MainWindow::on_checkBox_clicked(bool checked)
     ui->wSa->Setvw(checked);
 }
 
+void MainWindow::Progress(qint64 bytesSent, qint64 bytesTotal) {
+    qreal n = (qreal)bytesSent/(qreal)bytesTotal;
+    //ui->progressBar->setValue(n * 100);
+    qDebug() << "Uploaded " << bytesSent << " of " << bytesTotal;
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 #include "wsbora.h"
+#include <QHttpMultiPart>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QFile>
 
 namespace Ui {
 class MainWindow;
@@ -15,14 +19,22 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void Senthash();
+    void AddPart(QHttpMultiPart *multiPart,QString name, QByteArray by);
 
 private slots:
     void chimage(QByteArray bzip, QString hMD5);
     void on_checkBox_clicked(bool checked);
 
+    void Progress(qint64 bytesSent, qint64 bytesTotal);
+    void uploadDone();
 
 private:
     Ui::MainWindow *ui;
+
+private:
+    QNetworkReply * reply;
+    bool startUp;
 };
 
 #endif // MAINWINDOW_H
